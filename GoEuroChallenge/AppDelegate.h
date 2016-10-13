88a293de288f1462b92cef023a3dc1d0111d5e08
Anonym <asdf@me.com>
2016-10-13 11:49:15 +0200

    Initial Commit

---
+++ GoEuroChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoEuroChallenge
+//
+//  Created by Michal Tomaszewski on 13.10.2016.
+//  Copyright © 2016 Michal Tomaszewski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoEuroChallenge/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GoEuroChallenge
+//
+//  Created by Michal Tomaszewski on 13.10.2016.
+//  Copyright © 2016 Michal Tomaszewski. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+
+@end
